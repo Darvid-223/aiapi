37e38c8179f30eb95b1f199627567be580bfa283
Darvid-223 <david.arvidsson@hotmail.com>
2025-04-10 23:11:50 +0200

    🔧 Möjliggjorde åtkomst till Flask-appen via mobil genom att aktivera WiFi och köra på 0.0.0.0

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 debug_mode = os.getenv("FLASK_DEBUG", "false").lower() == "true"
+local_dev = os.getenv("LOCAL_DEV", "false").lower() == "true"
 
 # Starta Flask-appen
 if __name__ == "__main__":
-    app.run(debug=debug_mode)
+    if local_dev:
+        app.run(debug=debug_mode, host="0.0.0.0")
+    else:
+        app.run(debug=debug_mode)
